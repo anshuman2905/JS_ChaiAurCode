f032c03a58d44a3d6f38f32163bb353cd4cb8837
anshuman2905 <anshumaniiitbh@gmail.com>
2025-06-12 09:01:01 +0000

    VARIABLES DONE

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId= 144553
+let accountEmail="anshumaniii@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let userState
+
+// accountId=2
+
+accountEmail="hc@hc.com"
+accountPassword="212121"
+accountCity="Banda"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accountPassword,userState])
